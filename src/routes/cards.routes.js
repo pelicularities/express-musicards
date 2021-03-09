d9ad27d204e0a54b6719a35d3939c9617acceda5
Grace Teng <pelicularities@users.noreply.github.com>
2021-03-09 16:44:44 +0800

    Refactor cards routes into separate cards.routes

---
+++ src/routes/cards.routes.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const protectRoute = require("../middleware/protectRoute");
+const decksController = require("../controllers/decks.controller");
+const cardsController = require("../controllers/cards.controller");
+
+// ROUTES
+router.post("/", async (req, res, next) => {
+  const newCard = await cardsController.createOne(req.deckId, req.body, next);
+  if (newCard) res.status(201).send(newCard);
+});
+
+router.put("/:cardId", async (req, res, next) => {
+  const updatedCard = await cardsController.findByIdAndUpdate(
+    req.params.cardId,
+    req.body,
+    next
+  );
+  if (updatedCard) res.status(200).send(updatedCard);
+});
+
+router.delete("/:cardId", async (req, res, next) => {
+  const deletedCard = await cardsController.findByIdAndDelete(
+    req.params.cardId,
+    req.deckId,
+    next
+  );
+  res.status(200).send(deletedCard);
+});
+
+module.exports = router;
--- src/routes/decks.routes.js
+++ src/routes/decks.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const protectRoute = require("../middleware/protectRoute");
+const cardsRouter = require("../routes/cards.routes");
 const decksController = require("../controllers/decks.controller");
 const cardsController = require("../controllers/cards.controller");
 
@@ -31,15 +32,6 @@ router.post("/", async (req, res, next) => {
   if (newDeck) res.status(201).send(newDeck);
 });
 
-router.post("/:deckId/cards", async (req, res, next) => {
-  const newCard = await cardsController.createOne(
-    req.params.deckId,
-    req.body,
-    next
-  );
-  if (newCard) res.status(201).send(newCard);
-});
-
 router.put("/:deckId", async (req, res, next) => {
   const updatedDeck = await decksController.findByIdAndUpdate(
     req.params.deckId,
@@ -47,16 +39,6 @@ router.put("/:deckId", async (req, res, next) => {
     next
   );
   if (updatedDeck) res.status(200).send(updatedDeck);
-});
-
-// will refactor into separate cards.routes later
-router.put("/:deckId/cards/:cardId", async (req, res, next) => {
-  const updatedCard = await cardsController.findByIdAndUpdate(
-    req.params.cardId,
-    req.body,
-    next
-  );
-  if (updatedCard) res.status(200).send(updatedCard);
 });
 
 router.delete("/:deckId", async (req, res, next) => {
@@ -67,14 +49,14 @@ router.delete("/:deckId", async (req, res, next) => {
   res.status(200).send(deletedDeck);
 });
 
-// will refactor into separate cards.routes later
-router.delete("/:deckId/cards/:cardId", async (req, res, next) => {
-  const deletedCard = await cardsController.findByIdAndDelete(
-    req.params.cardId,
-    req.params.deckId,
-    next
-  );
-  res.status(200).send(deletedCard);
-});
+// NESTED CARDS ROUTER
+router.use(
+  "/:deckId/cards",
+  (req, res, next) => {
+    req.deckId = req.params.deckId;
+    next();
+  },
+  cardsRouter
+);
 
 module.exports = router;
